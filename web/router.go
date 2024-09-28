ae2eced6b8525c4f2a0ff46ca2d1e89045979802
noobdy <htommyf12@gmail.com>
2024-09-29 03:56:30 +0800

    added homework view

--- web/router.go
+++ web/router.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"net/http"
 
+	"github.com/is1ab/Arvosana/service/db"
 	"github.com/is1ab/Arvosana/service/logger"
 	"github.com/is1ab/Arvosana/web/template/pages"
 	"github.com/labstack/echo/v4"
@@ -19,6 +20,37 @@ func RegisterRouter(e *echo.Group) {
 		Root:       "static",
 		Filesystem: http.FS(static),
 	}))
+
+	e.GET("/homework/latest", func(c echo.Context) error {
+		ctx := c.Request().Context()
+		l := logger.Ctx(ctx)
+		q := db.Ctx(ctx)
+
+		hws, err := q.GetAllHomeworks(ctx)
+		if err != nil {
+			l.Errorln(err)
+			return echo.NewHTTPError(http.StatusInternalServerError)
+		}
+
+		_hws := make([]pages.Homework, len(hws))
+		for i := range _hws {
+			_hws[i].Name = hws[i].Name
+			_hws[i].BeginAt = hws[i].BeginAt
+			_hws[i].EndAt = hws[i].EndAt
+		}
+
+		html, err := Render(ctx, pages.Homeworks(_hws))
+		if err != nil {
+			l.Errorln(err)
+			return echo.NewHTTPError(http.StatusInternalServerError)
+		}
+
+		return c.HTML(http.StatusOK, html)
+	})
+
+	e.GET("/homework/:semester", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "")
+	})
 
 	e.GET("/test", func(c echo.Context) error {
 		ctx := c.Request().Context()
