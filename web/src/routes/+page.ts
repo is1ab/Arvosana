85ded398ff306e541fb5cd92ffe276da4f18a109
noobdy <htommyf12@gmail.com>
2024-11-03 04:04:35 +0800

    fix build

--- web/src/routes/+page.ts
+++ web/src/routes/+page.ts
@@ -1,16 +1,16 @@
 import type { PageLoad } from './$types';
 
 type GradeInfo = {
-    student_id: string;
-    grade: number;
+	student_id: string;
+	grade: number;
 };
 
 export const load: PageLoad = async ({ fetch }) => {
-    const t = new Date();
-    const semester = `${t.getFullYear()}${t.getMonth() > 9 ? 'f' : 's'}`;
+	const t = new Date();
+	const semester = `${t.getFullYear()}${t.getMonth() > 9 ? 'f' : 's'}`;
 
-    const res = await fetch(`/api/grade/${semester}/HW0`);
-    const info: GradeInfo[] = await res.json();
+	const res = await fetch(`/api/grade/${semester}/HW0`);
+	const info: GradeInfo[] = await res.json();
 
-    return { info, semester };
+	return { info, semester };
 };
